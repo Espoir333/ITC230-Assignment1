20ac6d9c4df798210a36612d6452f2414f37bf44
Espoir Makiese <44072742+Espoir333@users.noreply.github.com>
2019-06-13 23:33:03 -0700

    Create packages.js

---
+++ packages.js
@@ -0,0 +1,23 @@
+{
+  "name": "assignment1",
+  "version": "1.0.0",
+  "description": "Simple server for static file, using node.  Mostly copied from textbook",
+  "main": "simple-serve-static-file.js",
+  "scripts": {
+    "test": "node simple-serve-static-file"
+  },
+  "repository": {
+    "type": "git",
+    "url": "http://espoir333.club/Itc230/assignment1/index.html"
+  },
+  "keywords": [
+    "ITC230",
+    "Espoir",
+    "M",
+    "Makiese",
+    "assignment",
+    "1"
+  ],
+  "author": "Espoir N Makiese",
+  "license": "ISC"
+}
